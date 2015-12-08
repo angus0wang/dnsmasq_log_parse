88224146729885856ed96aceffbcc89aa0e33366
angus wang <angus0wang@163.com>
2015-12-08 20:12:40 +0800

    dig asynchronize with limit

--- dnsmasq_log_parse.js
+++ dnsmasq_log_parse.js
@@ -10,6 +10,7 @@ var fs = require('fs');
 var async = require('async');
 var execSync = require('child_process').execSync;
 var exec = require('child_process').exec;
+var Sync = require('sync');
 var ip_lists = fs.readFileSync(CHINA_IPLIST_FILE).toString().split("\n");
 var configed_domains_114_114_114_114 = execSync("sudo cat "+DNSMASQ_CONFIG_DIR+"*.conf | grep 114.114.114.114 | awk -F / '{print $2}'").toString().split("\n");
 var configed_domains_8_8_8_8 = execSync("sudo cat "+DNSMASQ_CONFIG_DIR+"*.conf | grep 8.8.8.8 | awk -F / '{print $2}'").toString().split("\n");
@@ -17,8 +18,8 @@ var configed_domains_8_8_8_8 = execSync("sudo cat "+DNSMASQ_CONFIG_DIR+"*.conf |
 var new_config_filename = DNSMASQ_CONFIG_DIR+execSync("date +'%Y-%m-%d'").toString().split("\n")[0]+'.conf';
 //var new_config_filename = execSync("date +'%Y-%m-%d'").toString().split("\n")[0]+'.conf';
 console.log(new_config_filename);
-
-
+var process_max = parseInt(process.argv[2])|| 100;
+//console.log(process_max);
 
 function checkip(ipaddress){
     var array_length=ip_lists.length;
@@ -37,42 +38,51 @@ function checkip(ipaddress){
     }
 }
 function checkdomain(domain){
-    if(configed_domains_114_114_114_114.indexOf(domain) == -1 && configed_domains_8_8_8_8.indexOf(domain) == -1){
-        exec("dig "+domain+"  | grep A |tail -n 1|awk '{print $NF}'",
-             function (error, stdout, stderr) {
-                 if (error !== null) {
-                 console.log('exec error: ' + error);
-                 }
-                 var host_ip = stdout.split('\n')[0];
-                 if(checkip(host_ip)){
-                     var new_domains = 'server=/'+domain+'/114.114.114.114\n';
-                 }else{
-                     var new_domains = 'server=/'+domain+'/8.8.8.8\n';
-                 }
-                 console.log(new_domains);
-                 fs.appendFile(new_config_filename, new_domains);
-
-             })
-    }
+    exec("dig "+domain+" @114.114.114.114  | grep A |tail -n 1|awk '{print $NF}'",
+         function (error, stdout, stderr) {
+             if (error !== null) {
+             console.log('exec error: ' + error);
+             }
+             var host_ip = stdout.split('\n')[0];
+             if(checkip(host_ip)){
+                 var new_domains = 'server=/'+domain+'/114.114.114.114\n';
+             }else{
+                 var new_domains = 'server=/'+domain+'/8.8.8.8\n';
+             }
+             console.log(new_domains);
+             fs.appendFile(new_config_filename, new_domains);
+         })
 
 }
 /*read log and backup log*/
 //execSync("sudo cp -f "+DNSMASQ_LOGFILE+"  .");
-execSync("sudo cat "+DNSMASQ_LOGFILE+"|grep forwarded|awk -F forwarded '{print $2}' | grep -v 114.114.114.114 |awk '{print $1}'|awk -F . '{print $(NF-1)\".\"$NF}'|sort|uniq | grep -v  \"^\\\.\"  > /tmp/host_ip.log");
+
+console.log('starting '+process_max+' records ......');
+
+execSync("sudo cat "+DNSMASQ_LOGFILE+"|grep forwarded|awk -F forwarded '{print $2}' | grep -v 114.114.114.114 |awk '{print $1}'|sort|uniq | egrep -v  \"^\\\.|in-addr\.arpa|/$\"  > /tmp/host_ip.log");
 //execSync("tar zcvf dnsmasq_`date +'%Y-%m-%d_%T'`.log.tgz dnsmasq.log; rm dnsmasq.log");
 
 var log_domains = fs.readFileSync('/tmp/host_ip.log').toString().split("\n");
-var list_length = log_domains.length-1;
 log_domains.pop();//remove empty element;
-var new_domains = new Array();
-console.log('starting...'+list_length+' records after 3 seconds');
-sleep.sleep(3);
-//for(var i=0,j=0;i<list_length-1;i++){//list_length-1 to cut the last empty record
-//    checkdomain(log_domains[i]);
-//    console.log(i);
-////    fs.appendFile(new_config_filename, 'bb');
+var list_length = log_domains.length;
+//var domains = new Array();
+for(var i=0;i<list_length;i++){
+    if(configed_domains_114_114_114_114.indexOf(log_domains[i]) != -1 || configed_domains_8_8_8_8.indexOf(log_domains[i]) != -1){
+        log_domains.splice(i,1);
+//        console.log(log_domains[i]);
+        i--;
+        list_length--;
+    }
+}
+//for(var i=0;i<list_length+process_max;i=i+process_max){
+//    var domains = log_domains.slice(i,i+process_max-1);
+//    async.eachLimit(log_domains,process_max,checkdomain,checkip);
+//    
+//    console.log(log_domains);
 //}
-async.each(log_domains,checkdomain,checkip);
+var domains = log_domains.slice(0,process_max-1);
+async.eachLimit(domains,process_max,checkdomain,checkip);
+
 execSync(CMD_STOP_DNSMASQ);
 execSync(CMD_START_DNSMASQ);
 //console.log(new_domains);
